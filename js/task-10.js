6c67b866fabc3c0ee3995e46d98a08268b5ed800
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-06 00:52:09 +0200

    add tast 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('[type=number]');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('div#boxes');
+
+btnCreateEl.addEventListener('click', createBoxes);
+btnDestroyEl.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const listItems = [];
+  
+  amount = Number(inputEl.value);
+  listItems.length = amount;
+  listItems.fill("");
+
+  listItems.map((newElement, index) => {
+    listItems.forEach((el) => {
+      newElement = document.createElement('div');
+      newElement.style.width = 30 + index * 10 + 'px';
+      newElement.style.height = 30 + index * 10 + 'px';
+      newElement.style.backgroundColor = getRandomHexColor();
+    });
+    listItems.push(newElement);
+  });
+
+  return boxesEl.append(...listItems);
+  };
+
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+  inputEl.value = "";
+};
+
+
